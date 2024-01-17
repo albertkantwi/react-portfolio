a77ee5851f8b6dcdd8da6c1d9c044428ba92f9e6
unknown <albertkantwi@gmail.com>
2024-01-17 22:43:33 +0000

    Add nav bar to project

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.css';
+import { MdOutlineHome, MdLaptopChromebook } from 'react-icons/md';
+import { LuUser2 } from 'react-icons/lu';
+import { RiServiceLine } from 'react-icons/ri';
+import { FaRegMessage } from 'react-icons/fa6';
 
-const Nav = () => (
-  <div>Nav</div>
-);
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
+  return (
+    <nav>
+      <a href="#"><MdOutlineHome /></a>
+      <a href="#about"><LuUser2 /></a>
+      <a href="#experience"><MdLaptopChromebook /></a>
+      <a href="#services"><RiServiceLine /></a>
+      <a href="#contact"><FaRegMessage /></a>
+    </nav>
+  );
+};
 
 export default Nav;
