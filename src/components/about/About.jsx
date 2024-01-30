d043b2073fbf59b5cd1f6c215bb26661c3622681
unknown <albertkantwi@gmail.com>
2024-01-30 22:38:12 +0000

    Update about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,41 +5,50 @@ import { MdWork } from 'react-icons/md';
 import { VscFolderLibrary } from 'react-icons/vsc';
 import Myself from '../../assets/Myself.jpeg';
 
-const About = () => (
-  <section>
-    <h5>Get to know</h5>
-    <h2>About me</h2>
-    <div className="container about-container">
-      <div className='about-me'>
-        <div className="about-me-img">
-          <img src={Myself} alt="myself" />
+const About = () => {
+  return (
+    <section>
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+
+      <div className="container about-container">
+        <div className="about-me">
+          <div className="about-me-img">
+            <img src={Myself} alt="myself" />
+          </div>
+        </div>
+
+        <div className="about-content">
+          <div className="about-cards">
+            <article className="about-card">
+              <FaAward />
+              <h2>Experience</h2>
+              <small>1+ years working</small>
+            </article>
+            <article className="about-card">
+              <MdWork />
+              <h2>Job type</h2>
+              <small>Remote collaboration</small>
+            </article>
+            <article className="about-card">
+              <VscFolderLibrary />
+              <h2>Projects</h2>
+              <small>40+ completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            quo repudiandae odio quod molestias adipisci assumenda, sed iste
+            omnis fuga? Eligendi autem eveniet sed nam! Nesciunt est earum nemo
+            a.
+          </p>
+          <a href="#contact" className="btn">
+            Get in Touch
+          </a>
         </div>
       </div>
-      <div>
-        <div className="about-cards">
-          <article className='about-card'>
-            <FaAward />
-            <h2>Experience</h2>
-            <small>1+ years working</small>
-          </article>
-          <article>
-            <MdWork />
-            <h2>Job type</h2>
-            <small>Remote collaboration</small>
-          </article>
-          <article>
-            <VscFolderLibrary />
-            <h2>Project</h2>
-            <small>40+ completed</small>
-          </article>
-        </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quo repudiandae odio quod molestias adipisci assumenda, sed iste omnis fuga? Eligendi autem eveniet sed nam! Nesciunt est earum nemo a.
-        </p>
-        <a href="#contact" className="btn">Get in Touch</a>
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default About;
