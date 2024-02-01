dcb633f276da1c2071e8d733ece487cb440547fb
unknown <albertkantwi@gmail.com>
2024-02-01 16:28:08 +0000

    Add portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import otherProjects from '../../data/projectsData';
 import './Portfolio.css';
 
-const Portfolio = () => (
-  <div>Portfolio</div>
-);
+const Portfolio = () => {
+  const [selectProject, setSelectProject] = useState({
+    title: '',
+    image: '',
+    description: '',
+    technologies: [],
+    link: [],
+  });
 
+  return(
+
+  <section>
+    <h5>My Recent Work</h5>
+    <h2>Portfolio</h2>
+
+    <div className="container portfolio-container">
+      {
+        otherProjects.length > 0 && otherProjects.map((project, index) => (
+          <article key={{index}} className="portfolio-item">
+            <div className="portfolio-img">
+              <img src={project.image} alt={project.title} />
+            </div>
+            <h3>{project.title}</h3>
+            <div className="btn-container">
+              <button className="btn project-btn">See Project</button>
+            </div>
+          </article>
+        )
+        )
+      }
+    </div>
+    
+  </section>
+);
+  };
 export default Portfolio;
