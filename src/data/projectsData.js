dc9c61940f1140081333fb6d06ab1e8c4269e1c1
unknown <albertkantwi@gmail.com>
2024-02-01 16:26:51 +0000

    Add data to make portfolio page dynamic

---
+++ src/data/projectsData.js
@@ -0,0 +1,54 @@
+import fashion from '../assets/fashion.png';
+
+const otherProjects = [
+  {
+    id: 1,
+    title: 'Profesional Art Printing Data',
+    image: fashion,
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    link: ['https://albertkantwi.github.io/Portfolio_Project/', 'https://github.com/albertkantwi/Portfolio_Project/'],
+  },
+  {
+    id: 2,
+    title: 'Profesional Art Printing Data',
+    image: fashion,
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    link: ['https://albertkantwi.github.io/Portfolio_Project/', 'https://github.com/albertkantwi/Portfolio_Project/'],
+  },
+  {
+    id: 3,
+    title: 'Profesional Art Printing Data',
+    image: fashion,
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    link: ['https://albertkantwi.github.io/Portfolio_Project/', 'https://github.com/albertkantwi/Portfolio_Project/'],
+  },
+  {
+    id: 4,
+    title: 'Profesional Art Printing Data',
+    image: fashion,
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    link: ['https://albertkantwi.github.io/Portfolio_Project/', 'https://github.com/albertkantwi/Portfolio_Project/'],
+  },
+  {
+    id: 5,
+    title: 'Profesional Art Printing Data',
+    image: fashion,
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    link: ['https://albertkantwi.github.io/Portfolio_Project/', 'https://github.com/albertkantwi/Portfolio_Project/'],
+  },
+  {
+    id: 6,
+    title: 'Profesional Art Printing Data',
+    image: fashion,
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    link: ['https://albertkantwi.github.io/Portfolio_Project/', 'https://github.com/albertkantwi/Portfolio_Project/'],
+  },
+];
+
+export default otherProjects;
