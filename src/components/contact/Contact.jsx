d7e565688d5f644100b38031669c807bae9cf4b9
unknown <albertkantwi@gmail.com>
2024-02-04 21:20:50 +0000

    Add contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,69 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './Contact.css';
+import emailjs from 'emailjs-com';
 
-const Contact = () => (
-  <div>Contact</div>
-);
+const Contact = () => {
+  const [messageSent, setMeassageSent] = useState('');
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_8re3rqj',
+        'template_g6dpjde',
+        form.current,
+        '6Z-d9bUjIqFCqFnzG',
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          if (result.text === 'OK') {
+            setMeassageSent(true);
+          }
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      );
+  };
+
+  return (
+    <section>
+      <h5>Get In Touch</h5>
+      <h2>Contact me</h2>
+
+      <div className="container contact-container">
+        <div className="contact-description">
+          <p>
+            I'm always interested in hearing about new projects, so if you'd
+            like to chat please get in touch.
+          </p>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Full Name" required />
+          <input type="email" name="email" placeholder=" Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Write me something..."
+            required
+          />
+          {messageSent && <p>Message successfuly sent</p>}
+          {messageSent === false && (
+            <p>
+              Oops! Could not sent your message, please check your connection
+              and try again!
+            </p>
+          )}
+          <button type="submit" className="btn">
+            Get in touch
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default Contact;
