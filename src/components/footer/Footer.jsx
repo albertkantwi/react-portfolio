2c787049e4aa83a18492653c3a2177b7aac17b07
unknown <albertkantwi@gmail.com>
2024-02-06 02:24:44 +0000

    Add footer to project

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import './Footer.css';
+import { IoLogoGithub } from "react-icons/io5";
+import { TfiLinkedin } from "react-icons/tfi";
+import { FaAngellist } from "react-icons/fa";
+import { IoLogoMedium } from "react-icons/io5";
 
 const Footer = () => (
-  <div>Footer</div>
+  <footer>
+    <div>
+    <hr className="line" />
+    <ul className="footer-label">
+      <li><a href="#header">Home</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+    </div>
+    <div className="footer-social">
+      <a href="https://www.linkedin.com/"><IoLogoGithub /></a>
+      <a href="https://www.linkedin.com/"><TfiLinkedin /></a>
+      <a href="https://www.linkedin.com/"><FaAngellist /></a>
+      <a href="https://www.linkedin.com/"><IoLogoMedium /></a>
+    </div>
+  </footer>
 );
 
 export default Footer;
