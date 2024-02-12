d31acd8ed18ddcda11c0522e5b2cb3c4b335e992
unknown <albertkantwi@gmail.com>
2024-02-12 20:13:52 +0000

    Add button images

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,38 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Modal.css';
+import { LuArrowUpRightFromCircle } from "react-icons/lu";
+import { PiGithubLogoFill } from "react-icons/pi";
 
 const Modal = ({ projectInfo, onClose }) => (
   <div className="modal-container">
     <div className="modal-page">
       <div className="title-container">
-        <h3 className="modal-title">{projectInfo.title}</h3>
-        <ul className="modal-description">
+        <h3>{projectInfo.title}</h3>
+        <ul>
           {projectInfo.technologies.map((tech, index) => (
             <li key={index}>{tech}</li>
           ))}
         </ul>
         <img src={projectInfo.image} alt={projectInfo.title} />
       </div>
-      <div>
+      <div className="description-container">
         <p>{projectInfo.description}</p>
-        <div>
-          <button type="button">
+        <div className="popup-btn">
+          <button type="button" className="btn">
             <a href={projectInfo.link[0]} rel="noreferrer">
               <span>See Live</span>
+              <LuArrowUpRightFromCircle />
             </a>
           </button>
-          <button type="button">
+          <button type="button" className="btn">
             <a href={projectInfo.link[1]} rel="noreferrer">
               <span>See Source</span>
+              <PiGithubLogoFill />
             </a>
           </button>
-          <div>
+        </div>
+        <div>
             <button type="button" onClick={onClose}>
               X
             </button>
           </div>
-        </div>
       </div>
     </div>
   </div>
