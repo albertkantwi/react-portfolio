e8d8258cdf6a564298d21b03661e44f52a8301e1
unknown <albertkantwi@gmail.com>
2024-02-20 22:51:09 +0000

    Add smaller device version to makee portfolio page responsive

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -20,8 +20,8 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio-container">
-        {otherProjects.length > 0 &&
-          otherProjects.map((project, index) => (
+        {otherProjects.length > 0 
+        && otherProjects.map((project, index) => (
             <article key={index} className="portfolio-item">
               <div className="portfolio-img">
                 <img src={project.image} alt={project.title} />
