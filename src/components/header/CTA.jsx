4de3526e8198e998ca886ff66cd6e490cc8033f3
unknown <albertkantwi@gmail.com>
2024-03-05 00:57:35 +0000

    Update resume link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CTA = () => (
   <div className="cta">
-    <a href="https://docs.google.com/document/d/1GHciterdRl_dPvVWe3F0mKzjxSbENw58HGHSNRMNIFQ/edit?usp=sharing" target="_blank" rel="noreferrer" className="btn">My resume</a>
+    <a href="https://docs.google.com/document/d/1h6wWK1UsMEdJ3jg2SXVumd141sZswCRg2zziQVAT2FA/edit?usp=sharing" target="_blank" rel="noreferrer" className="btn">My resume</a>
     <a href="#contact" className="btn">Get in touch</a>
   </div>
 );
